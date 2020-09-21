d30476e2fba8bd0576e89ba6e909cd7d50a96133
AndyGeek <andy.eulogio@gmail.com>
2020-09-21 00:16:33 -0500

    Use server navigation

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <menu>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </menu>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
